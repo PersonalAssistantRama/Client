d81630040b094e698629de70fd3ebe9fcad79a17
Zena <zena.vilenia@gmail.com>
2018-05-18 20:52:12 +0700

    voice regocnition done

--- android/app/src/main/java/com/client/MainApplication.java
+++ android/app/src/main/java/com/client/MainApplication.java
@@ -3,10 +3,12 @@ package com.client;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import net.no_mad.tts.TextToSpeechPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.wmjmc.reactspeech.VoicePackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new TextToSpeechPackage(),
+          new VoicePackage()
       );
     }
 
